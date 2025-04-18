8c6c21a62a345ed336303aa13baf565beb3b7508
NiteCity <95667743+nitecity@users.noreply.github.com>
2025-04-18 18:50:32 +0000

    fifth

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const symbol = 'BTCUSDT';
-const limit = 1000;
+const limit = 5;
 const url = 'https://fapi.binance.com';
 const path = `/fapi/v1/depth?symbol=${symbol}&limit=${limit}`;
+
+
+async function send(){
+    try {
+        const response = await fetch(url+path);
+        const data = await response.json();
+        console.log(data);
+    } catch(error){
+        console.error('Error: ', error);
+    }
+}
+
+send()
