a487bbac79d2fdd26a4ddb17d785cb18ffab5157
NiteCity <95667743+nitecity@users.noreply.github.com>
2025-04-19 13:01:37 +0000

    seventh

--- test.js
+++ test.js
@@ -1,3 +1,55 @@
-x = '98501.99999'
+const WebSocket = require('ws');
+const socket = new WebSocket('wss://fstream.binance.com/ws/btcusdt@depth@500ms');
 
-console.log(parseInt(x));
+socket.onopen = () => {
+    console.log('Connected to server');
+};
+
+socket.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+
+    const asks = {};
+    for (let [key, value] of data.a) {
+        key = parseInt(key)
+        value = Number(value);
+        if (asks[key]) {
+            asks[key] += value;
+        } else {
+            asks[key] = value;
+        }
+    }
+
+    console.log(Object.keys(asks).length);
+    
+    const bids = {};
+    for (let [key,value] of data.b){
+        key = parseInt(key);
+        value = Number(value);
+        if (bids[key]){
+            bids[key] += value;
+        }else {
+            bids[key] = value;
+        }
+    }
+
+    console.log('----------------')
+    console.log('Asks:');
+    for (const prop in asks){
+        value = asks[prop];
+        
+        console.log(`Price: ${prop} ---- Size: ${parseInt(value * prop).toLocaleString()} USDT`);
+    }
+
+    console.log('----------------')
+    console.log('Bids:');
+    for (const prop in bids){
+        value = bids[prop];
+        
+        console.log(`Price: ${prop} ---- Size: ${parseInt(value * prop).toLocaleString()} USDT`);
+    }
+
+};
+
+socket.onerror = (err) => {
+    console.error('WebSocket error:', err);
+};
