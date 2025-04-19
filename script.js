a23a3175d682c9577a2b1215a6c1e3f9a8d1739c
NiteCity <95667743+nitecity@users.noreply.github.com>
2025-04-19 11:23:49 +0000

    sixth

--- script.js
+++ script.js
@@ -1,17 +1,64 @@
 const symbol = 'BTCUSDT';
-const limit = 5;
+const limit = 1000;
 const url = 'https://fapi.binance.com';
 const path = `/fapi/v1/depth?symbol=${symbol}&limit=${limit}`;
 
 
-async function send(){
-    try {
-        const response = await fetch(url+path);
-        const data = await response.json();
-        console.log(data);
-    } catch(error){
-        console.error('Error: ', error);
-    }
+async function getData(){
+  try {
+      const response = await fetch(url+path);
+      const data = await response.json();
+      const asks = data.asks;
+      const bids = data.bids;
+      design(asks, bids);
+      
+  } catch(error){
+      console.error('Error: ', error);
+  }
 }
 
-send()
+getData()
+
+function design(asks, bids){
+  const remapAsks = {};
+  const remapBids = {};
+
+  for(let [price, size] of asks){
+    price = parseInt(price);
+    size = Number(size);
+
+    if (remapAsks[price]){
+      remapAsks[price] += size;
+    } else {
+      remapAsks[price] = size;
+    }
+  }
+
+  for(let [price,size] of bids){
+    price = parseInt(price);
+    size = Number(size);
+
+    if (remapBids[price]) {
+      remapBids[price] += size;
+    } else {
+      remapBids[price] = size;
+    }
+  }
+
+  const resultAsks = Object.entries(remapAsks).map(([key, value]) => [parseInt(key), value]);
+  const resultBids = Object.entries(remapBids).map(([key, value]) => [parseInt(key), value]);
+  resultAsks.reverse();
+  resultBids.reverse();
+  
+  resultAsks.forEach(([price, size]) => {
+    const integer = parseInt(size * price);
+    console.log(`Price: ${price} ---- Size: ${integer.toLocaleString()} USDT`);
+  });
+  console.log('Asks:');
+  console.log('Bids:');
+  resultBids.forEach(([price, size]) => {
+    const integer = parseInt(size * price);
+    console.log(`Price: ${price} ---- Size: ${integer.toLocaleString()} USDT`);
+  });
+
+}
---
+++ test.js
@@ -0,0 +1,3 @@
+x = '98501.99999'
+
+console.log(parseInt(x));
