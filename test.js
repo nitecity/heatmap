b02eeb969a338b019801478e77679312f8335baa
NiteCity <95667743+nitecity@users.noreply.github.com>
2025-04-21 09:23:51 +0000

    12

--- test.js
+++ test.js
@@ -75,83 +75,83 @@ function startAggTrades() {
     };
 }
 
-// function stopAggTrades() {
-//     socket.close();
-//     console.log('Disconnected from server');
-//     // html logic here
-// }
+function stopAggTrades() {
+    socket.close();
+    console.log('Disconnected from server');
+    // html logic here
+}
 
-// function startHeatmap() {
-//     getData();
-//     setInterval(() => {
-//         getData();
-//     }, 20000);
-// }
+function startHeatmap() {
+    getData();
+    setInterval(() => {
+        getData();
+    }, 20000);
+}
 
-// function stopHeatmap() {
-//     // stop the heatmap
-// }
+function stopHeatmap() {
+    // stop the heatmap
+}
 
-// async function getData(){
-//     const symbol = 'BTCUSDT';
-//     const limit = 1000;
-//     const url = 'https://fapi.binance.com';
-//     const path = `/fapi/v1/depth?symbol=${symbol}&limit=${limit}`;
-//     try {
-//         const response = await fetch(url+path);
-//         const data = await response.json();
-//         const asks = data.asks;
-//         const bids = data.bids;
-//         design(asks, bids);
+async function getData(){
+    const symbol = 'BTCUSDT';
+    const limit = 1000;
+    const url = 'https://fapi.binance.com';
+    const path = `/fapi/v1/depth?symbol=${symbol}&limit=${limit}`;
+    try {
+        const response = await fetch(url+path);
+        const data = await response.json();
+        const asks = data.asks;
+        const bids = data.bids;
+        design(asks, bids);
         
-//     } catch(error){
-//         console.error('Error: ', error);
-//     }
-// }
+    } catch(error){
+        console.error('Error: ', error);
+    }
+}
 
-// function design(asks, bids){
-//     const remapAsks = {};
-//     const remapBids = {};
+function design(asks, bids){
+    const remapAsks = {};
+    const remapBids = {};
 
-//     for(let [price, size] of asks){
-//         price = parseInt(price);
-//         size = parseFloat(size);
+    for(let [price, size] of asks){
+        price = parseInt(price);
+        size = parseFloat(size);
 
-//         if (remapAsks[price]){
-//             remapAsks[price] += size;
-//         } else {
-//             remapAsks[price] = size;
-//         }
-//     }
+        if (remapAsks[price]){
+            remapAsks[price] += size;
+        } else {
+            remapAsks[price] = size;
+        }
+    }
 
-//     for(let [price,size] of bids){
-//         price = parseInt(price);
-//         size = parseFloat(size);
+    for(let [price,size] of bids){
+        price = parseInt(price);
+        size = parseFloat(size);
 
-//         if (remapBids[price]) {
-//             remapBids[price] += size;
-//         } else {
-//             remapBids[price] = size;
-//         }
-//     }
+        if (remapBids[price]) {
+            remapBids[price] += size;
+        } else {
+            remapBids[price] = size;
+        }
+    }
 
-//     const resultAsks = Object.entries(remapAsks).map(([key, value]) => [parseInt(key), value]);
-//     const resultBids = Object.entries(remapBids).map(([key, value]) => [parseInt(key), value]);
-//     resultAsks.reverse();
-//     resultBids.reverse();
+    const resultAsks = Object.entries(remapAsks).map(([key, value]) => [parseInt(key), value]);
+    const resultBids = Object.entries(remapBids).map(([key, value]) => [parseInt(key), value]);
+    resultAsks.reverse();
+    resultBids.reverse();
     
-//     resultAsks.forEach(([price, size]) => {
-//         const sum = parseInt(size * price);
-//         console.log(`Price: ${price} ---- Size: ${sum.toLocaleString()} USDT`);
-//     });
-//     console.log('Asks:');
-//     console.log('Bids:');
-//     resultBids.forEach(([price, size]) => {
-//         const sum = parseInt(size * price);
-//         console.log(`Price: ${price} ---- Size: ${sum.toLocaleString()} USDT`);
-//     });
+    resultAsks.forEach(([price, size]) => {
+        const sum = parseInt(size * price);
+        console.log(`Price: ${price} ---- Size: ${sum.toLocaleString()} USDT`);
+    });
+    console.log('Asks:');
+    console.log('Bids:');
+    resultBids.forEach(([price, size]) => {
+        const sum = parseInt(size * price);
+        console.log(`Price: ${price} ---- Size: ${sum.toLocaleString()} USDT`);
+    });
 
-// }
+}
 
 
 //startAggTrades();
