af23d5d86823bffbb6e247c5bd8595b0ef67dde1
Kev <agh4masood@gmail.com>
2025-04-21 14:11:09 +0200

    23

--- script.js
+++ script.js
@@ -1,64 +1,172 @@
-const symbol = 'BTCUSDT';
-const limit = 1000;
-const url = 'https://fapi.binance.com';
-const path = `/fapi/v1/depth?symbol=${symbol}&limit=${limit}`;
+const aggStartButton = document.getElementById('aggStartButton');
+// const heatmapStartButton = document.getElementById('heatmapStartButton');
+// const aggStopButton = document.getElementById('aggStopButton');
+// const heatmapStopButton = document.getElementById('heatmapStopButton');
+const container = document.getElementById('container');
 
+function startAggTrades() {
+    const socket = new WebSocket('wss://fstream.binance.com/ws/btcusdt@aggTrade');
+    socket.onopen = () => {
+        console.log('Connected to server');
+    };
 
-async function getData(){
-  try {
-      const response = await fetch(url+path);
-      const data = await response.json();
-      const asks = data.asks;
-      const bids = data.bids;
-      design(asks, bids);
-      
-  } catch(error){
-      console.error('Error: ', error);
-  }
+    socket.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        
+        if (data.e == 'aggTrade') {
+            const price = parseFloat(data.p);
+            const size = parseFloat(data.q);
+            const time = new Date(data.T).toLocaleTimeString();
+            const sum = parseInt(size * price);
+            const text = `Price: ${price} ---------- Amount: ${sum.toLocaleString()} $ ---------- Time: ${time}`;
+
+            if (data.m){
+                const header = 'Aggressive Sell';
+                
+                if ( sum >= 100000 && sum <= 300000) {
+                    updateElements(header, text, 'rgb(53, 1, 1)');
+                } else if (sum > 300000 && sum <= 600000) {
+                    updateElements(header, text, 'rgb(101, 2, 2)');
+                } else if (sum > 600000 && sum <= 1000000) {
+                    updateElements(header, text, 'rgb(151, 6, 6)');
+                } else if (sum > 1000000) {
+                    updateElements(header, text, 'rgb(255, 7, 7)');
+                }
+
+                
+            } else{
+                const header = 'Aggressive Buy';
+                
+                if ( sum >= 100000 && sum <= 300000) {
+                    updateElements(header, text, 'rgb(1, 53, 1)');
+                } else if (sum > 300000 && sum <= 600000) {
+                    updateElements(header, text, 'rgb(2, 101, 2)');
+                } else if (sum > 600000 && sum <= 1000000) {
+                    updateElements(header, text, 'rgb(6, 151, 6)');
+                } else if (sum > 1000000) {
+                    updateElements(header, text, 'rgb(7, 255, 7)');
+                }
+                
+            }
+            
+        }
+        
+
+    };
+
+    socket.onerror = (err) => {
+        console.error('WebSocket error:', err);
+    };
 }
 
-getData()
+function stopAggTrades() {
+    socket.close();
+    console.log('Disconnected from server');
+    // html logic here
+}
+
+function startHeatmap() {
+    getData();
+    setInterval(() => {
+        getData();
+    }, 20000);
+}
+
+function stopHeatmap() {
+    // stop the heatmap
+}
+
+async function getData(){
+    const symbol = 'BTCUSDT';
+    const limit = 1000;
+    const url = 'https://fapi.binance.com';
+    const path = `/fapi/v1/depth?symbol=${symbol}&limit=${limit}`;
+    try {
+        const response = await fetch(url+path);
+        const data = await response.json();
+        const asks = data.asks;
+        const bids = data.bids;
+        design(asks, bids);
+        
+    } catch(error){
+        console.error('Error: ', error);
+    }
+}
 
 function design(asks, bids){
-  const remapAsks = {};
-  const remapBids = {};
+    const remapAsks = {};
+    const remapBids = {};
 
-  for(let [price, size] of asks){
-    price = parseInt(price);
-    size = parseFloat(size);
+    for(let [price, size] of asks){
+        price = parseInt(price);
+        size = parseFloat(size);
 
-    if (remapAsks[price]){
-      remapAsks[price] += size;
-    } else {
-      remapAsks[price] = size;
+        if (remapAsks[price]){
+            remapAsks[price] += size;
+        } else {
+            remapAsks[price] = size;
+        }
     }
-  }
 
-  for(let [price,size] of bids){
-    price = parseInt(price);
-    size = parseFloat(size);
+    for(let [price,size] of bids){
+        price = parseInt(price);
+        size = parseFloat(size);
 
-    if (remapBids[price]) {
-      remapBids[price] += size;
-    } else {
-      remapBids[price] = size;
+        if (remapBids[price]) {
+            remapBids[price] += size;
+        } else {
+            remapBids[price] = size;
+        }
     }
-  }
 
-  const resultAsks = Object.entries(remapAsks).map(([key, value]) => [parseInt(key), value]);
-  const resultBids = Object.entries(remapBids).map(([key, value]) => [parseInt(key), value]);
-  resultAsks.reverse();
-  resultBids.reverse();
-  
-  resultAsks.forEach(([price, size]) => {
-    const sum = parseInt(size * price);
-    console.log(`Price: ${price} ---- Size: ${sum.toLocaleString()} USDT`);
-  });
-  console.log('Asks:');
-  console.log('Bids:');
-  resultBids.forEach(([price, size]) => {
-    const sum = parseInt(size * price);
-    console.log(`Price: ${price} ---- Size: ${sum.toLocaleString()} USDT`);
-  });
+    const resultAsks = Object.entries(remapAsks).map(([key, value]) => [parseInt(key), value]);
+    const resultBids = Object.entries(remapBids).map(([key, value]) => [parseInt(key), value]);
+    resultAsks.reverse();
+    resultBids.reverse();
+    
+    resultAsks.forEach(([price, size]) => {
+        const sum = parseInt(size * price);
+        console.log(`Price: ${price} ---- Size: ${sum.toLocaleString()} USDT`);
+    });
+    console.log('Asks:');
+    console.log('Bids:');
+    resultBids.forEach(([price, size]) => {
+        const sum = parseInt(size * price);
+        console.log(`Price: ${price} ---- Size: ${sum.toLocaleString()} USDT`);
+    });
 
+}
+
+
+//startAggTrades();
+//startHeatmap();
+
+aggStartButton.addEventListener('click', (e) => {
+    
+        startAggTrades();
+    // } else if (e.target === aggStopButton) {
+    //     stopAggTrades();
+    // } else if (e.target === heatmapStartButton) {
+    //     startHeatmap();
+    // } else if (e.target === heatmapStopButton) {
+    //     stopHeatmap();
+    // }
+});
+
+
+function updateElements(header, text, bg){
+    const newData = document.createElement('div');
+    newData.style.backgroundColor = bg;
+    newData.style.borderRadius = '5px';
+    newData.style.padding = '10px';
+    newData.style.margin = '5px';
+    newData.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.1)';
+    newData.style.fontSize = '14px';
+    newData.innerHTML = header + '<br/>' + text;
+    //newData.style.color = 'white';
+    newData.style.fontWeight = 'bold';
+    container.prepend(newData);
+    while(container.children.length > 10) {
+        container.removeChild(container.lastChild);
+    }
 }
